6453f75e58a9add39ef1dcb348bac9f2c3e846b0
Brandon Oakes <brandontoakes@gmail.com>
2025-01-07 10:41:02 -0500

    init and mod1

---
+++ Module1.py
@@ -0,0 +1,9 @@
+var1 = 8
+var2 = 19
+var3 = 99
+var4 = "eight"
+var5 = "Baylor"
+
+print("1) Let's party at " + var5 + "like it's " + str(var2)+str(var3) + ".")    #Let’s party at Baylor like it’s 1999.
+print("2) The sum of " + str(var1) + " and " + str(var2) + " is " + str(var1 + var2) + ".")    #The sum of 8 and 19 is 27.
+print("3) " + var4 + var4 + var4 + "could be written as " + str(var1) + str(var1) + str(var1))    #eighteighteight could be written as 888.
