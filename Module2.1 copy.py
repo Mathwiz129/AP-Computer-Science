05504848eac3fb828063ae74b9475a8ff72ea2ee
Brandon Oakes <brandontoakes@gmail.com>
2025-01-13 14:52:10 -0500

    mod 2.2 rename

--- Module2.1 copy.py
+++
@@ -1,4 +0,0 @@
-num1 = int(input("First Number:"))
-num2 = int(input("Second Number:"))
-num3 = int(input("Third Number:"))
-print("The average of " + str(num1) + ", " + str(num2) + ", and " + str(num3) + " is " + str((num1 + num2 + num3)/3)+ ".")
