e5d91a4fd603e5b8cdd99fb57ae652af4f090f76
Brandon Oakes <brandontoakes@gmail.com>
2025-01-16 09:58:00 -0500

    Quizzes

---
+++ Quiz1.1.py
@@ -0,0 +1,3 @@
+num1 = input("Integer:")
+num2 = input("Number That Contains a Decimal (float):")
+print("The sum of " + num1 + " and " + num2 + " is " + str(int(num1) + float(num2)))
---
+++ Quiz1.2.py
@@ -0,0 +1,7 @@
+num1 = 1000
+num2 = 900
+num3 = 70
+num4 = 9
+year = num1 + num2 + num3 + num4 
+
+print("Mr.Young was born in " + str(year) + ". He is " + str(2025 - year) + " years old.")
