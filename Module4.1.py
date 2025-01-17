1fdb33137ae54956237f99f26e9b5e1581e637e9
Brandon Oakes <brandontoakes@gmail.com>
2025-01-17 13:28:51 -0500

    module4

---
+++ Module4.1.py
@@ -0,0 +1,2 @@
+list1 = ["Baylor","School", 2023, 2024, 2025, 2026]
+print("I go to the " + list1[0] + " "+ list1[1] + ", and I will graduate in '" + str(list1[5])[2:4] + ".")
---
+++ Module4.2.py
@@ -0,0 +1,4 @@
+list1 = ["Baylor", "School", 2023, 2024, 2025]
+list1 = list1[:2]
+list1.extend(["Chattanooga", 2023, 2024]) 
+print(list1)
