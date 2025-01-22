fe9d3ca6e53ca90ea67d71799c7bcd83254eaa21
Brandon Oakes <brandontoakes@gmail.com>
2025-01-22 08:51:23 -0500

    Module 5

---
+++ Module5.1.py
@@ -0,0 +1,4 @@
+var1 = "Brandon"
+var2 = "Oakes"
+if (len(var1) > 4):
+    print(var1)
---
+++ Module5.2.py
@@ -0,0 +1,6 @@
+var1 = "Brandon"
+var2 = "Oakes"
+if (len(var1) > 4):
+    print(var1)
+else: 
+    print(var2)
